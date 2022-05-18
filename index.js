b66a1f79cbb8ec6e48533dbc796a49a8c3a2df44
bgtdinh <bgtdinh@gmail.com>
2022-05-17 18:27:18 -0700

    Added initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+module.exports = app;
+
